d4b14b06eafc156e9bb7ea6f7b826f2908d9d956
Anthony Broome <anthonybroome87@gmail.com>
2017-03-13 11:48:09 +0000

    VM first attempt

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_network_interface" "nic" {
+    name = "${var.name}_nic0"
+    location = "${var.allspark["location"]}"
+    resource_group_name = "${var.allspark["resource_group_name"]}"
+
+    ip_configuration {
+        name = "${var.name}_ip"
+        subnet_id = "${var.allspark["subnet_id"]}"
+        private_ip_address_allocation = "dynamic"
+    }
+}
+
+resource "azurerm_virtual_machine" "test" {
+    name = "${var.name}"
+    location = "${var.allspark["location"]}"
+    resource_group_name = "${var.allspark["resource_group_name"]}"
+    network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+    vm_size = "${var.image}"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "16.04-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "${var.name}_os"
+        vhd_uri = "${var.allspark["storage_account_endpoint"]}${var.allspark["storage_container_name"]}/${var.name}_os_disk.vhd"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name = "${var.name}"
+        admin_username = "${var.username}"
+        admin_password = "${var.password}"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+
+        ssh_keys {
+          path = "/home/admin/.ssh/authorized_keys"
+          key_data = "${var.allspark["ssh_public"]}"
+        }
+    }
+
+    tags {
+        environment = "${var.allspark["resource_group_name"]}"
+        role = "${var.role}"
+        os = "${var.os}"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "allspark" {
+  description = "allspark network and storage details"
+}
+
+variable "name" {
+  description = "Hostname"
+}
+
+variable "username" {
+  description = "Admin Username"
+}
+
+variable "password" {
+  description = "Admin Password"
+}
+
+variable "image" {
+  description = "Azure VM Image type"
+  default     = "Standard_D2_v2"
+}
+
+variable "role" {
+  description = "Server Role"
+  default     = "none"
+}
+
+variable "os" {
+  description = "Operating System"
+  default     = "ubuntu"
+}
