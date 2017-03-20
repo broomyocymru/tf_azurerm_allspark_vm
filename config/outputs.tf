01b1e55302a900d218cae0d981ee4bf6ba9aa2c2
Anthony Broome <anthonybroome87@gmail.com>
2017-03-20 12:30:43 +0000

    index issue

--- config/outputs.tf
+++ config/outputs.tf
@@ -1,19 +1,19 @@
 output "image" {
-  value = "${element(split(",",lookup(var.options, var.key) ,0))}"
+  value = "${element(split(",",lookup(var.options, var.key),1))}"
 }
 
 output "publisher" {
-  value = "${element(split(", ",lookup(var.options, var.key),1))}"
+  value = "${element(split(",",lookup(var.options, var.key),2))}"
 }
 
 output "offer" {
-  value = "${element(split(", ",lookup(var.options, var.key),2))}"
+  value = "${element(split(",",lookup(var.options, var.key),3))}"
 }
 
 output "sku" {
-  value = "${element(split(", ",lookup(var.options, var.key),3))}"
+  value = "${element(split(",",lookup(var.options, var.key),4))}"
 }
 
 output "version" {
-  value = "${element(split(", ",lookup(var.options, var.key),4))}"
+  value = "${element(split(",",lookup(var.options, var.key),5))}"
 }
--- config/variables.tf
+++ config/variables.tf
@@ -7,7 +7,7 @@ variable "options" {
   description = "Predefined hardware/os combos."
   type = "map"
   default = {
-    "std_windows_server" = "Standard_D2_v2, MicrosoftWindowsServer, WindowsServer, 2012-R2-Datacenter, latest"
-    "std_ubuntu_server" = "Standard_D2_v2, Canonical, UbuntuServer, 16.04-LTS, latest"
+    "std_windows_server" = "Standard_D2_v2,MicrosoftWindowsServer,WindowsServer,2012-R2-Datacenter, latest"
+    "std_ubuntu_server" = "Standard_D2_v2,Canonical, UbuntuServer,16.04-LTS,latest"
   }
 }
